5e9eb8f16d21f4e3a3a11ba5898068c76ea1f593
Alan Ye <git@wrye.dev>
2024-08-28 19:50:16 +0800

    refactor: remove unused pages

--- src/consts.ts
+++ src/consts.ts
@@ -18,16 +18,6 @@ export const BLOG: Metadata = {
   DESCRIPTION: "A collection of articles on topics I am passionate about.",
 };
 
-export const WORK: Metadata = {
-  TITLE: "Work",
-  DESCRIPTION: "Where I have worked and what I have done.",
-};
-
-export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
-};
-
 export const SOCIALS: Socials = [
   { 
     NAME: "GitHub",
